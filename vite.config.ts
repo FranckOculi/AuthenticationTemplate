3a517c91a56b310fe82e11f5f7481cb9c6088f15
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-03-12 11:37:33 +0100

    add test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './setupTest.ts',
 	},
 })
