bd5d785405124da610bd060b47e9a94c78a2c5de
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-03-12 14:32:24 +0100

    add component test : Spinner

---
+++ src/components/ui/Spinner/Spinner.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { describe, expect, test } from 'vitest'
+import { render } from '@testing-library/react'
+import Spinner from './Spinner'
+
+describe('Spinner', () => {
+	test('it should render successfully', async () => {
+		const { baseElement } = render(<Spinner />)
+
+		expect(baseElement).toBeInTheDocument()
+	})
+})
