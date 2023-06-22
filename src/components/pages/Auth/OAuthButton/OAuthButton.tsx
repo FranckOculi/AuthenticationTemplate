424b55753af48206f44ac6859f33d1305b792648
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-06-22 13:00:26 +0200

    OAuthButton Update : add microsoftProvider

--- src/components/pages/Auth/OAuthButton/OAuthButton.tsx
+++ src/components/pages/Auth/OAuthButton/OAuthButton.tsx
@@ -1,11 +1,15 @@
 import style from './OAuthButton.module.scss'
 
-import microsoft from '../../../../assets/img/microsoft.png'
-import google from '../../../../assets/img/google.png'
+import { useState } from 'react'
+
+import microsoft from '@/assets/img/microsoft.png'
+import google from '@/assets/img/google.png'
+
+import MicrosoftProvider from '../MicrosoftProvider'
 
 const OAuth2Namespace = {
-	microsoft: microsoft,
-	google: google,
+	microsoft,
+	google,
 }
 
 type nameProps = keyof typeof OAuth2Namespace
@@ -15,10 +19,18 @@ type Props = {
 }
 
 const OAuthButton = ({ name }: Props) => {
+	const [initMicrosoftProcess, setMicrosoftProcess] = useState(false)
+	const [initGoogleProcess, setGoogleProcess] = useState(false)
+
+	const handleLogin = () => {
+		if (name === 'microsoft') setMicrosoftProcess(!initMicrosoftProcess)
+	}
+
 	return (
-		<div className={`${style.main} ${style[name]} `}>
+		<div className={`${style.main} ${style[name]} `} onClick={handleLogin}>
 			<img src={OAuth2Namespace[name]} className={style.image} />
 			<span>Sign in with {name[0].toUpperCase() + name.slice(1)}</span>
+			{initMicrosoftProcess && <MicrosoftProvider />}
 		</div>
 	)
 }
