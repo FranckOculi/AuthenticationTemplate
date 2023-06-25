55c6f9a9f4943d6c9ac25d4bf4fc0f2c0a4537e5
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-06-25 09:25:21 +0200

    .env type Update : add azure variables

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,8 +3,10 @@
 interface ImportMetaEnv {
 	readonly VITE_APP_VITE_URLTITLE: string
 	readonly VITE_TOKEN_KEY: string
+	readonly VITE_AZURE_CLIENT_ID: string
+	readonly VITE_AZURE_TENANT_ID: string
+	readonly VITE_OAUTH_REDIRECT_URI: string
 }
-
 interface ImportMeta {
 	readonly env: ImportMetaEnv
 }
