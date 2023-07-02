de50334f29e7b4592faf6c139362c153bdc9d7bb
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-07-02 09:07:31 +0200

    OAuthButton Update : add MicrosoftProvider

--- src/components/pages/Auth/OAuthButton/OAuthButton.tsx
+++ src/components/pages/Auth/OAuthButton/OAuthButton.tsx
@@ -1,11 +1,7 @@
 import style from './OAuthButton.module.scss'
 
-import { useState } from 'react'
-
 import microsoft from '@/assets/img/microsoft.png'
 import google from '@/assets/img/google.png'
-
-import MicrosoftProvider from '../MicrosoftProvider'
 
 const OAuth2Namespace = {
 	microsoft,
@@ -16,21 +12,14 @@ type nameProps = keyof typeof OAuth2Namespace
 
 type Props = {
 	name: nameProps
+	onClick: (e?: React.MouseEvent<HTMLDivElement>) => void
 }
 
-const OAuthButton = ({ name }: Props) => {
-	const [initMicrosoftProcess, setMicrosoftProcess] = useState(false)
-	const [initGoogleProcess, setGoogleProcess] = useState(false)
-
-	const handleLogin = () => {
-		if (name === 'microsoft') setMicrosoftProcess(!initMicrosoftProcess)
-	}
-
+const OAuthButton = ({ name, onClick }: Props) => {
 	return (
-		<div className={`${style.main} ${style[name]} `} onClick={handleLogin}>
+		<div className={`${style.main} ${style[name]} `} onClick={onClick}>
 			<img src={OAuth2Namespace[name]} className={style.image} />
 			<span>Sign in with {name[0].toUpperCase() + name.slice(1)}</span>
-			{initMicrosoftProcess && <MicrosoftProvider />}
 		</div>
 	)
 }
