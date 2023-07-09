278637caec5047b76d0c9a7735748746dde571a8
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-07-09 07:26:32 +0200

    env.d.ts Update : delete urltitle variable

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_APP_VITE_URLTITLE: string
 	readonly VITE_TOKEN_KEY: string
 	readonly VITE_AZURE_CLIENT_ID: string
 	readonly VITE_AZURE_TENANT_ID: string
